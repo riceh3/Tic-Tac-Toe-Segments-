902bf11cd284ef89f6f3a85fb8e9870b191807e0
riceh3 <riceh3@uni.coventry.ac.uk>
2015-10-29 13:21:59 +0000

    Menu is finished

--- menu- instructions.py
+++ menu- instructions.py
@@ -13,5 +13,23 @@ confirm = input("type yes or no")
 if confirm == "no":
     print ("Not to worry here are the instruction")
 
-    print(" 
+    print(" The Game will ask you to select a box on the grid, where you want to place your 'x' or '0'")
+    print(" A guid of the grid will appear at the start of the game")
+
+    print(" You will then enter your 'x' or '0', and it will place the mark")
+
+    print("The aim of the game, is to get your mark to fill three boxes in a row")
+yes = "yes"
+for y in yes:
+    if confirm == "yes":
+        continue
+else:
+    print("Lets Play")
+
+# Then instructions will only show if the user selects that they have not
+# - playerd the game before
+
+
+
+    
     
