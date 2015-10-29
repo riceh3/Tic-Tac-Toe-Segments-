cd548b501df9dd90fd840e904e13648106f6c7f8
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2015-10-29 10:16:35 +0000

    Create Board - Nathan.py

---
+++ Board - Nathan.py
@@ -0,0 +1,11 @@
+r1 = ["_", "_", "_"]
+r2 = ["_", "_", "_"]
+r3 = ["_", "_", "_"]
+
+
+
+#   Prints the tic-tac-toe grid
+    print('')
+    print('\t\t', '\t'.join(r1), '\n')  |
+    print('\t\t', '\t'.join(r2), '\n')  |----- each list is joined into a one string but each seperation made by '\t' (tab space)
+    print('\t\t', '\t'.join(r3), '\n')  |
