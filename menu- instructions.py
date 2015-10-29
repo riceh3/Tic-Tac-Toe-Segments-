a499250822a897e33ec7e9b30582b600c3508904
riceh3 <riceh3@uni.coventry.ac.uk>
2015-10-29 10:51:17 +0000

    Made a start to the menu

---
+++ menu- instructions.py
@@ -0,0 +1,17 @@
+# Game Menu - Hannah 
+
+print(" Tic Tac Toe")
+name = input("Hey whats your name?")
+
+#takes user name and asigns to player one
+
+print("Hi there, " + name + " have you played this game before")
+confirm = input("type yes or no")
+
+# Instruction Menu
+
+if confirm == "no":
+    print ("Not to worry here are the instruction")
+
+    print(" 
+    
