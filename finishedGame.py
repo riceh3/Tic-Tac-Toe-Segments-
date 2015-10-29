d860a6bc7e2ad00a199402d7e41821b9c669e698
riceh3 <riceh3@uni.coventry.ac.uk>
2015-10-29 13:23:36 +0000

    Added the menu to the start of the finished game

--- finishedGame.py
+++ finishedGame.py
@@ -1,8 +1,38 @@
 # This is the full game of Tic Tac Toe : all segements uploaded will be
 #  - uploaded to this file to run the full game.
 
-# Game Board - Hannah 
+# Game Menu - Hannah 
 
+print(" Tic Tac Toe")
+name = input("Hey whats your name?")
+
+#takes user name and asigns to player one
+
+print("Hi there, " + name + " have you played this game before")
+confirm = input("type yes or no")
+
+# Instruction Menu
+
+if confirm == "no":
+    print ("Not to worry here are the instruction")
+
+    print(" The Game will ask you to select a box on the grid, where you want to place your 'x' or '0'")
+    print(" A guid of the grid will appear at the start of the game")
+
+    print(" You will then enter your 'x' or '0', and it will place the mark")
+
+    print("The aim of the game, is to get your mark to fill three boxes in a row")
+yes = "yes"
+for y in yes:
+    if confirm == "yes":
+        continue
+else:
+    print("Lets Play")
+
+# Then instructions will only show if the user selects that they have not
+# - playerd the game before
+
+# Game Board - Hannah
 board = [0,1,2,
          3,4,5,
          6,7,8,
