51b96d853bd79929a373cea702548e7e4ec58f88
riceh3 <riceh3@uni.coventry.ac.uk>
2015-10-29 10:08:33 +0000

    Completed game board function
    can now call the game board throughout the game to show to the players

---
+++ gameBoard.py
@@ -0,0 +1,17 @@
+# Game Board
+
+board = [0,1,2,
+         3,4,5,
+         6,7,8,
+        ]
+""" The Board Function"""
+def grid():
+    print (board[0], " | ", board[1], " | ", board[2])
+    print ("--------------- ")
+    print (board[3], " | ", board[4], " | ", board[5])
+    print ("---------------")
+    print (board[6], " | ", board[7], " | ", board[8])
+
+grid()
+    
+    
