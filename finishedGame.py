c36ea481cbaef8e32d6ee1937acf3818d6d1f062
riceh3 <riceh3@uni.coventry.ac.uk>
2015-11-02 12:48:55 +0000

    added the ai (now working) to the game

--- finishedGame.py
+++ finishedGame.py
@@ -31,6 +31,20 @@ for y in yes:
 else:
     print("Lets Play")
 
+# Selction of number of players for menu - Hannah
+# Leads to 2 player or one player code
+
+players = input("How many players, type 1 or 2")
+
+if players == "1":
+    print("you will now play against the computer")
+elif players == "2":
+    print("Two players")
+else:
+    print("Sorry that is not an option ")
+    
+print(name + " is player one")
+
 # Then instructions will only show if the user selects that they have not
 # - playerd the game before
 
@@ -39,6 +53,23 @@ board = [0,1,2,
          3,4,5,
          6,7,8,
         ]
+
+# Ai for one player - Hannah
+""" This will generate a random number between zero and eight, then be used as player two box"""
+
+global playerTwo
+global aiPlayer
+import random
+
+def computer():
+    global aiPlayer
+    import random
+    ai = [0,1,2,3,4,5,6,7,8]
+    aiPlayer = ()
+    aiPlayer = random.choice(ai)
+
+    
+        
 """ The Board Function"""
 def grid():
     print (board[0], " | ", board[1], " | ", board[2])
@@ -49,43 +80,70 @@ def grid():
 
 grid()
 
+
+if players == "2":
+    print(name + " you are x")
+    print("player 2 is 0")
+elif players == "1":
+    print(name + " you are x")
+    print("the computer is o")
+else:
+    print("restart the game")
+
 # Ai for one player - Hannah
 
     
 #   The program checks if the specified row and column is out of range,
 #   once user input is recieved - Nathan
 
+
 while True:
-# Ai for one player - Hannah
-""" This will generate a random number between zero and eight, then be used as player two box"""
-    global playerTwo
-    import random
-    def computer():
-        import random
-        playerTwo = random.randint(0,8)
-
+    if players == 2:
+        print(name + "turn")
+        playerOne = input("pick a box")
+        playerOne = int(playerOne)
+        if board[playerOne] != "x" and board[playerOne] != "0":
+            board[playerOne] = input("X or O? ")
+        else:
+            print("\nWhoa, sorry dude. Out of range.\n")
+        grid()
         
-    playerOne = input("Player 1 pick a box")
-    playerOne = int(playerOne)
-    
-# if the row and column specification is within range of the grid
-    if board[playerOne] != "x" and board[playerOne] != "0":
-        board[playerOne] = input("X or O? ")
-    else:
-        print("\nWhoa, sorry dude. Out of range.\n")
-    grid()
-    
-#   Checks if a place in the game grid has already been filled
-#   from the last player's turn
+        print("Player 2 turn")
+        playerTwo = input("Pick a box")
+        playerTwo = int(playerTwo)
+        if board[playerTwo] != "x" and board[playerTwo] != "0":
+            board[playerTwo] = input("x or 0")
+        else:
+            print("\nPlace is already taken. Try again\n")
+        grid()
 
-    playerTwo = input("Player 2 pick a box")
-    playerTwo = int(playerTwo)
-    
-    if board[playerTwo] != "x" and board[playerTwo] != "0":
-        board[playerTwo] = input("x or 0")
+    elif players == "1":
+        print(name + " turn")
+        playerOne = input(" Pick a box")
+        playerOne = int(playerOne)
+        if board[playerOne] != "x" and board[playerOne] != "0":
+            board[playerOne] = input(" type your mark")
+        else:
+            print("That box is taken")
+        grid()
+        
+        print("The computers turn")
+        computer()
+        compTwo = aiPlayer
+        compTwo = int(compTwo)
+        if board[compTwo] != "x" and board[compTwo] != "0":
+            board[compTwo] = input("type 0")
+            grid()
+        else:
+            computer()
+            grid()
     else:
-        print("\nPlace is already taken. Try again\n")
-    grid()
+        print(" Next go")
+        
+#Checks if a place in the game grid has already been filled
+#from the last player's turn
+
+
 
 # runs the input choice again if the user has chosen a spot out of the grid range
 
