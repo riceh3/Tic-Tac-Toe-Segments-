fa11754403e0ddcaa2d9d3f49f0836b2693a9f2d
riceh3 <riceh3@uni.coventry.ac.uk>
2015-11-02 09:57:39 +0000

    player selection options - Hannah

---
+++ playerSelection-Hannah.py
@@ -0,0 +1,28 @@
+# Selction of number of players for menu - Hannah
+# Leads to 2 player or one player code
+
+players = input("How many players, type 1 or 2")
+
+if players == "1":
+    print("you will now play against the computer")
+elif players == "2":
+    print("Two players")
+else:
+    print("Sorry that is not an option ")
+    
+print(name + "is player one")
+
+
+
+# Put this code above while loop
+
+if players == "2":
+    print(name + "you are x")
+    print("player 2 is 0")
+elif players == "1":
+    print(name + "you are x")
+    print("the computer is o")
+else:
+    print("restart the game")
+    
+    
